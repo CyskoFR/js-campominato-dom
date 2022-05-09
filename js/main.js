f7ae358b6186f07a613d434de23d553a55fb8032
CyskoFR <franc.sko91@gmail.com>
2022-05-09 17:13:16 +0200

    add base static game

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 // I numeri non possono essere duplicati.
+
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
@@ -10,4 +11,77 @@
 // con difficoltà 0 => tra 1 e 100
 // con difficoltà 1 => tra 1 e 80
 // con difficoltà 2 => tra 1 e 50
+
+
+let slotNumber = 0;
+const numbers = [];
+const mainGameBox = document.getElementById("main-game_box");
+
+function changeDifficulty1() {
+    return slotNumber = 100;
+}
+function changeDifficulty2() {
+    return slotNumber = 80;
+}
+function changeDifficulty3() {
+    return slotNumber = 50;
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+while (numbers.length < 16) {
+    let num = getRandomNumber(1, 100);
+    if (!numbers.includes(num)) {
+        numbers.push(num);
+    }
+    
+}
+console.log(numbers);
+
+function startGame() {
+    if (slotNumber === 100 || slotNumber === 80 || slotNumber === 50) {
+        document.getElementById("game-start_box").style.display = "none";
+    } else {
+        alert("Select difficulty level")
+    }
+    for (let i = 1; i <= slotNumber; i++) {
+        const slot = document.createElement("div");
+        slot.classList.add("slot");
+        mainGameBox.append(slot)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// const list = document.querySelector(".list");
+
+// for( let i = 1; i <= 100; i++ ) {
+//     const element = document.createElement("li");
+
+//     element.classList.add("item");
+//     element.innerText = i;
+//     list.append(element);
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         element.innerText = "FizzBuzz"
+//         element.style.backgroundColor = "#f0466f"
+//     }   else if (i % 5 === 0) {
+//         element.innerText = "Buzz"
+//         element.style.backgroundColor = "#ffd166"
+//     }   else if (i % 3 === 0) {
+//         element.innerText = "Fizz"
+//         element.style.backgroundColor = "#0bd6a1"
+//     }
+// }
+
 
